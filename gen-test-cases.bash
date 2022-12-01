8032e5e063012ac07705e02856a8b964eb1b8f55
Filippo Di Pietro <41949805+dippi9845@users.noreply.github.com>
2022-12-01 17:31:10 +0000

    foder update to gen-tests

--- gen-test-cases.bash
+++ gen-test-cases.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 for ((i=100; i<=20000; i=$i+100)); do
     for ((j=50; j<=1000; j=$j+50)); do
-        ./sph $i $j > test-$i-$j.txt
+        bin/sph $i $j > tests/test-$i-$j.txt
     done
 done

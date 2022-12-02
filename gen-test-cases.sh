f4f0865f77552c4292d69e52c8152ee79e4d6dec
Filippo Di Pietro <filippodp9845@gmail.com>
2022-12-02 12:10:41 +0100

    changed extension from bash to sh

--- gen-test-cases.sh
+++ gen-test-cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i=100; i<=20000; i=$i+100)); do
+for ((i=100; i<=10000; i=$i+100)); do
     for ((j=50; j<=1000; j=$j+50)); do
         bin/sph $i $j > tests/test-$i-$j.txt
     done

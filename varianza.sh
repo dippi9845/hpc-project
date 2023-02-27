cd69db73dd7700bf2cf383ad5cb9e03fbaf21d91
Filippo Di Pietro <filippodp9845@gmail.com>
2023-02-27 16:49:56 +0100

    ora fa due cicli per quallo ad un thread e quello a 12

--- varianza.sh
+++ varianza.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+CORES=1
 for (( i=1000; $i<20000; i=$i*2 )); do
-    OMP_NUM_THREADS=1 $1 $i 100 > "varianza_${i}_100_1.txt"
+    OMP_NUM_THREADS=${CORES} $1 $i 1 > "varianza_${i}_${CORES}.txt"
+done
+
+CORES=12
+for (( i=1000; $i<20000; i=$i*2 )); do
+    OMP_NUM_THREADS=${CORES} $1 $i 1 > "varianza_${i}_${CORES}.txt"
 done

80175df5c9afcbbba9ab19200c8e9503e90b0a5a
Filippo Di Pietro <filippodp9845@gmail.com>
2023-02-27 18:14:41 +0100

    aggiunta variabile per gli steps

--- varianza.sh
+++ varianza.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+STEPS=100
 CORES=1
 for (( i=1000; $i<20000; i=$i*2 )); do
-    OMP_NUM_THREADS=${CORES} $1 $i 1 > "varianza_${i}_${CORES}.txt"
+    OMP_NUM_THREADS=${CORES} $1 $i ${STEPS} > "varianza_${i}_${CORES}.txt"
 done
 
 CORES=12
 for (( i=1000; $i<20000; i=$i*2 )); do
-    OMP_NUM_THREADS=${CORES} $1 $i 1 > "varianza_${i}_${CORES}.txt"
+    OMP_NUM_THREADS=${CORES} $1 $i ${STEPS} > "varianza_${i}_${CORES}.txt"
 done

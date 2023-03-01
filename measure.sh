75bd51cb9b23eed1831b1f7c05116c17bccd3917
Filippo Di Pietro <filippodp9845@gmail.com>
2023-03-02 00:45:11 +0100

    aggiunta funzione allo script bash

--- measure.sh
+++ measure.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+test_loop() {
+    EXE_NAME=$1
+    MAKE_NAME=$2
+
+    CURRENT_DIR="${EXPORT_PATH}${EXE_NAME}"
+    CURRENT_EXE="${EXE_PATH}/${EXE_NAME}"
+
+    make $MAKE_NAME 1>/dev/null 2>&1 
+    mkdir $CURRENT_DIR 2>/dev/null
+
+    for (( CUR_PAR=2000; $CUR_PAR<=$MAX_PARTICLES; CUR_PAR=$CUR_PAR+2000 )); do # particles [2000, 20000] -> 10
+        echo "[P: $CUR_PAR]"
+        for (( CUR_STEP=200; $CUR_STEP<=$MAX_STEPS; CUR_STEP=$CUR_STEP+200 )); do # steps [200, 1000] -> 5
+            TO_PRINT=""
+            for (( try=0; $try<$REPETITIONS; try=$try+1 )); do #  * 8 
+                OUT=`$CURRENT_EXE $CUR_PAR $CUR_STEP`
+                TO_PRINT="$TO_PRINT;$OUT"
+            done
+            echo ${TO_PRINT:1} > "$CURRENT_DIR/run_p_${CUR_PAR}_s_${CUR_STEP}${3}.csv"
+        done
+    done
+}
+
 EXPORT_PATH=/media/dippi/Volume1/hpc_tests/
 MAX_THREAD=24
 REPETITIONS=8
@@ -7,24 +30,25 @@ EXE_PATH=bin
 MAX_STEPS=1000
 MAX_PARTICLES=20000
 
-CURRENT_DIR="${EXPORT_PATH}sph"
-CURRENT_EXE="${EXE_PATH}/sph"
+#test_loop "sph" "sph"
 
-make sph 1>/dev/null 2>&1 
-mkdir $CURRENT_DIR
+#echo "Finito la versione seriale"
 
-for (( CUR_PAR=2000; $CUR_PAR<=$MAX_PARTICLES; CUR_PAR=$CUR_PAR+2000 )); do # particles [2000, 20000] -> 10
-    for (( CUR_STEP=200; $CUR_STEP<=$MAX_STEPS; CUR_STEP=$CUR_STEP+200 )); do # steps [200, 1000] -> 5
-        for (( try=0; $try<$REPETITIONS; try=$try+1 )); do #  * 8
-            #$CURRENT_EXE $CUR_PAR 
-            echo "[par: $CUR_PAR; steps: $CUR_STEP; R $try;]"
-        done
-    done
-done
+#test_loop "sph-simd" "simd"
+
+#echo "Finito la versione simd"
+
+echo "inizio omp"
 
 for (( i=1; $i<=$MAX_THREAD; i=$i+1 )); do
-    for (( try=1; $try<=$REPETITIONS; try=$try+1 )); do
-        echo "[runno con $i ths la $try volta ]"
-    done
-    echo
+    test_loop "sph-omp" "omp" "t_${i}"
 done
+
+echo "fine omp"
+echo "inizio omp simd"
+
+for (( i=1; $i<=$MAX_THREAD; i=$i+1 )); do
+    test_loop "sph-omp" "omp" "t_${i}"
+done
+
+echo "fine omp simd"

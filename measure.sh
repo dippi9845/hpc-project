38c89a1d06900a7da85a06efaee59b2677f64510
Filippo Di Pietro <filippodp9845@gmail.com>
2023-03-02 02:20:48 +0100

    aggiunta funzione per la versione parallela

--- measure.sh
+++ measure.sh
@@ -10,7 +10,7 @@ test_loop() {
     make $MAKE_NAME 1>/dev/null 2>&1 
     mkdir $CURRENT_DIR 2>/dev/null
 
-    for (( CUR_PAR=2000; $CUR_PAR<=$MAX_PARTICLES; CUR_PAR=$CUR_PAR+2000 )); do # particles [2000, 20000] -> 10
+    for (( CUR_PAR=500; $CUR_PAR<=$MAX_PARTICLES; CUR_PAR=$CUR_PAR+500 )); do # particles [2000, 20000] -> 10
         echo "[P: $CUR_PAR]"
         for (( CUR_STEP=200; $CUR_STEP<=$MAX_STEPS; CUR_STEP=$CUR_STEP+200 )); do # steps [200, 1000] -> 5
             TO_PRINT=""
@@ -23,12 +23,37 @@ test_loop() {
     done
 }
 
+test_loop_parallel() {
+    EXE_NAME=$1
+    MAKE_NAME=$2
+    TH=$3
+
+    CURRENT_DIR="${EXPORT_PATH}${EXE_NAME}"
+    CURRENT_EXE="${EXE_PATH}/${EXE_NAME}"
+
+    make $MAKE_NAME 1>/dev/null 2>&1 
+    mkdir $CURRENT_DIR 2>/dev/null
+
+    for (( CUR_PAR=500; $CUR_PAR<=$MAX_PARTICLES; CUR_PAR=$CUR_PAR+500 )); do # particles [2000, 20000] -> 10
+        echo "        [P: $CUR_PAR]"
+        for (( CUR_STEP=100; $CUR_STEP<=$MAX_STEPS; CUR_STEP=$CUR_STEP+50 )); do # steps [200, 1000] -> 5
+            TO_PRINT=""
+            for (( try=0; $try<$REPETITIONS; try=$try+1 )); do #  * 8 
+                OUT=`OMP_NUM_THREADS=${3} $CURRENT_EXE $CUR_PAR $CUR_STEP`
+                #echo "$CURRENT_EXE $CUR_PAR $CUR_STEP"
+                TO_PRINT="$TO_PRINT;$OUT"
+            done
+            echo ${TO_PRINT:1} > "$CURRENT_DIR/run_p_${CUR_PAR}_s_${CUR_STEP}_t_${TH}.csv"
+        done
+    done
+}
+
 EXPORT_PATH=/media/dippi/Volume1/hpc_tests/
 MAX_THREAD=24
 REPETITIONS=8
 EXE_PATH=bin
-MAX_STEPS=1000
-MAX_PARTICLES=20000
+MAX_STEPS=200
+MAX_PARTICLES=6000
 
 #test_loop "sph" "sph"
 
@@ -41,14 +66,16 @@ MAX_PARTICLES=20000
 echo "inizio omp"
 
 for (( i=1; $i<=$MAX_THREAD; i=$i+1 )); do
-    test_loop "sph-omp" "omp" "t_${i}"
+    echo "    [th: $i]" 
+    test_loop_parallel "sph-omp" "omp" $i
 done
 
 echo "fine omp"
 echo "inizio omp simd"
 
 for (( i=1; $i<=$MAX_THREAD; i=$i+1 )); do
-    test_loop "sph-omp" "omp" "t_${i}"
+    echo "    [th: $i]" 
+    test_loop_parallel "sph-omp-simd" "omp-simd" $i
 done
 
 echo "fine omp simd"

e2951460f62b6723b9ff2fa23e786dc36f0aec7a
Filippo Di Pietro <filippodp9845@gmail.com>
2023-03-06 20:45:07 +0100

    creato script per unire i dati seriali

---
+++ combine_result.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# comando per pescare i campi: cut -d";" -f FIELD_NUM FILE
+
+serial_union() {
+
+    SERIAL="${EXPORT_PATH}sph"
+    SERIAL_SIMD="${EXPORT_PATH}sph-simd"
+    SERIAL_OUTPUT="${EXPORT_PATH}serial-simd.csv"
+
+    RAW=""
+    for (( CUR_PAR=500; $CUR_PAR<=$MAX_PARTICLES; CUR_PAR=$CUR_PAR+500 )); do # particles [2000, 20000] -> 10
+        for (( CUR_STEP=200; $CUR_STEP<=$MAX_STEPS; CUR_STEP=$CUR_STEP+50 )); do # steps [200, 200] -> 1
+            SERIAL_FILE="${SERIAL}/run_p_${CUR_PAR}_s_${CUR_STEP}.csv"
+            SERIAL_SIMD_FILE="${SERIAL_SIMD}/run_p_${CUR_PAR}_s_${CUR_STEP}.csv"
+            if [ -e $SERIAL_FILE -a -e $SERIAL_SIMD_FILE ]; then
+                RAW="${RAW};Seriale Particelle: ${CUR_PAR};Seriale SIMD Particelle: ${CUR_PAR}"
+            fi
+        done
+    done
+    RAW="${RAW:1}\n" # salto il primo ;
+
+    for (( try=1; $try<=$REPETITIONS; try=$try+1 )); do
+        ROW=""
+        for (( CUR_PAR=500; $CUR_PAR<=$MAX_PARTICLES; CUR_PAR=$CUR_PAR+500 )); do # particles [2000, 20000] -> 10
+            for (( CUR_STEP=200; $CUR_STEP<=$MAX_STEPS; CUR_STEP=$CUR_STEP+50 )); do # steps [200, 200] -> 1
+                SERIAL_FILE="${SERIAL}/run_p_${CUR_PAR}_s_${CUR_STEP}.csv"
+                SERIAL_SIMD_FILE="${SERIAL_SIMD}/run_p_${CUR_PAR}_s_${CUR_STEP}.csv"
+                if [ -e $SERIAL_FILE -a -e $SERIAL_SIMD_FILE ]; then
+                    timeing1=`cut -d";" -f $try $SERIAL_FILE`
+                    timeing2=`cut -d";" -f $try $SERIAL_SIMD_FILE`
+                    ROW="${ROW};${timeing1};${timeing2}"
+                fi
+            done
+        done
+        RAW="${RAW}${ROW:1}\n"
+    done
+
+    echo -n -e $RAW > $SERIAL_OUTPUT
+
+}
+
+EXPORT_PATH=/media/dippi/Volume1/hpc_tests/
+MAX_THREAD=24
+REPETITIONS=8
+EXE_PATH=bin
+MAX_STEPS=200
+MAX_PARTICLES=6000
+
+serial_union
+

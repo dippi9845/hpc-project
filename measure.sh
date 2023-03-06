e6077c22eacfd508ef8a74a5b9f0b415886e5cfb
Filippo Di Pietro <filippodp9845@gmail.com>
2023-03-06 22:18:46 +0100

    aggiunta la misurazione di nvidia

--- measure.sh
+++ measure.sh
@@ -7,12 +7,12 @@ test_loop() {
     CURRENT_DIR="${EXPORT_PATH}${EXE_NAME}"
     CURRENT_EXE="${EXE_PATH}/${EXE_NAME}"
 
-    make $MAKE_NAME 1>/dev/null 2>&1 
-    mkdir $CURRENT_DIR 2>/dev/null
+    make $MAKE_NAME
+    mkdir $CURRENT_DIR
 
-    for (( CUR_PAR=500; $CUR_PAR<=$MAX_PARTICLES; CUR_PAR=$CUR_PAR+500 )); do # particles [2000, 20000] -> 10
+    for (( CUR_PAR=1000; $CUR_PAR<=$MAX_PARTICLES; CUR_PAR=$CUR_PAR+1000 )); do # particles [1000, 20000] -> 20
         echo "[P: $CUR_PAR]"
-        for (( CUR_STEP=200; $CUR_STEP<=$MAX_STEPS; CUR_STEP=$CUR_STEP+50 )); do # steps [200, 200] -> 1
+        for (( CUR_STEP=100; $CUR_STEP<=100; CUR_STEP=$CUR_STEP+50 )); do # steps [100, 100] -> 1
             TO_PRINT=""
             for (( try=0; $try<$REPETITIONS; try=$try+1 )); do #  * 8 
                 OUT=`$CURRENT_EXE $CUR_PAR $CUR_STEP`
@@ -53,9 +53,11 @@ MAX_THREAD=24
 REPETITIONS=8
 EXE_PATH=bin
 MAX_STEPS=200
-MAX_PARTICLES=6000
+MAX_PARTICLES=20000
 
-test_loop "sph" "sph"
+echo "Inizio Versione seriale"
+
+#test_loop "sph" "sph"
 
 echo "Finito la versione seriale"
 
@@ -80,3 +82,8 @@ echo "inizio omp"
 
 echo "fine omp simd"
 
+echo "Inizio cuda"
+
+test_loop "sph-cuda" "cuda"
+
+echo "Fine cuda"

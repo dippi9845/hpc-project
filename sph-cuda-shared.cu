76228c7a7b6bd36032fb946dbf73c8d794743c4d
Filippo Di Pietro <filippodp9845@gmail.com>
2023-03-13 14:56:03 +0100

    aggiunti newline nell'host

--- sph-cuda-shared.cu
+++ sph-cuda-shared.cu
@@ -464,25 +464,25 @@ int main(int argc, char **argv)
     for (int s=0; s<nsteps; s++) {
         double start = hpc_gettime();
 
-        printf("Pressione e Densita:\n");
+        printf("\nPressione e Densita:\n");
 
         compute_density_pressure<<<block_num, BLKDIM>>>(d_rho, d_pos_x, d_pos_y, d_p, n);
         
         cudaDeviceSynchronize();
 
-        printf("Forze:\n");
+        printf("\nForze:\n");
 
         compute_forces<<<block_num, BLKDIM>>>(d_rho, d_pos_x, d_pos_y, d_p, d_vx, d_vy, d_fx, d_fy, n);
 
         cudaDeviceSynchronize();
 
-        printf("Integrazione");
+        printf("\nIntegrazione\n");
 
         integrate<<<block_num, BLKDIM>>>(d_rho, d_pos_x, d_pos_y, d_vx, d_vy, d_fx, d_fy, n);
 
         cudaDeviceSynchronize();
 
-        printf("Reduction");
+        printf("\nReduction\n");
 
         reduction<<<block_num, BLKDIM>>>(d_vx, d_vy, n, d_sums);
         /* the average velocities MUST be computed at each step, even
